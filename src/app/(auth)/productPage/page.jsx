6b4ad792e745ebc02f43771440f0e51636bd51d2
Ayush Sharma <ayush.sharma74892@gmail.com>
2024-03-21 19:31:38 +0000

    hero page additions

---
+++ src/app/(auth)/productPage/page.jsx
@@ -0,0 +1,5 @@
+export default function productPage(){
+    return(
+        <h1 className="text-center">product page</h1>
+    );
+}
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,19 @@
-import Link from "next/link";
-export default function Home() {
+import Slideshow from '@/components/hero/Slider'
+import Link from 'next/link'
+export default function rootLayout() {
   return (
     <>
-      <h1 className="text-center text-7xl">homepage</h1>
+      <Slideshow />
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <Link rel="stylesheet" href="/productPage" ><button className="btn btn-primary">Buy Now</button></Link>
+          </div>
+        </div>
+      </div>
     </>
-  );
+  )
 }
