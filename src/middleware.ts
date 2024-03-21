a58eb6fda41d13b23aae4e260e1c1fd6385f5b8f
Ayush Sharma <ayush.sharma74892@gmail.com>
2024-03-21 14:54:18 +0000

    auth changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,19 +5,12 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
-  // publicRoutes: ['/anyone-can-visit-this-route'],
+  // publicRoutes: ['/'],
   // Prevent the specified routes from accessing
   // authentication information:
-  // ignoredRoutes: ['/no-auth-in-this-route'],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)"],
 });
  
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
- 
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
-  ]
+  matcher: [ "/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"]
 };
